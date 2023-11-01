136ac2f34ecc27212b0a658b01258fb8850a8a7e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-01 22:20:50 +0700

    add connection pool settings to GetConnection function

---
+++ database.go
@@ -0,0 +1,29 @@
+package golang_mysql
+
+import (
+	"database/sql"
+	"time"
+)
+
+// SetMaxIdleConns sets the maximum number of idle connections in the connection pool.
+
+// SetMaxOpenConns sets the maximum number of open (in-use and idle) connections in the pool.
+
+// SetConnMaxIdleTime sets the maximum amount of time a connection can remain idle before it's closed and removed from the pool.
+
+// SetConnMaxLifetime sets the maximum lifespan of a connection. After this duration, a connection is closed and removed from the pool.
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/golang_test")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
