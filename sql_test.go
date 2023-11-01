648cae88a39999389f759320fcc7b23154e4c5b9
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-01 22:34:52 +0700

    make a simple sql execution using ExecContext

---
+++ sql_test.go
@@ -0,0 +1,23 @@
+package golang_mysql
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	ctx := context.Background()
+
+	defer db.Close()
+
+	query := "INSERT INTO customer(id, name) VALUES ('1', 'goyoonjung')"
+	_, err := db.ExecContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success: Customer data inserted")
+}
