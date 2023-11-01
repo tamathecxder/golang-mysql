5c188ec444ba0ca381fc8563129f5989fe5cbac3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-01 22:52:50 +0700

    runs the select query and displays the retrieved data

--- sql_test.go
+++ sql_test.go
@@ -21,3 +21,34 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Success: Customer data inserted")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	ctx := context.Background()
+
+	defer db.Close()
+
+	query := "SELECT * FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	fmt.Println("Success: Query executed. Customer data retrieved.")
+
+	for rows.Next() {
+		var id, name string
+
+		err := rows.Scan(&id, &name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
+}
