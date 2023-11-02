2979912c4dfd6dd7634fb4baf966fd0f335756b1
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-02 18:31:23 +0700

    handling complex mysql column type in go

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -50,5 +51,46 @@ func TestQuerySql(t *testing.T) {
 
 		fmt.Println("Id:", id)
 		fmt.Println("Name:", name)
+	}
+}
+
+func TestComplexQuery(t *testing.T) {
+	db := GetConnection()
+	ctx := context.Background()
+
+	defer db.Close()
+
+	query := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	fmt.Println("Success: Query executed. Customer data retrieved.")
+
+	for rows.Next() {
+		var id, name, email string
+		var balance int32
+		var rating float64
+		var married bool
+		var birthDate, createdAt time.Time
+
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		fmt.Println("Email:", email)
+		fmt.Println("Balance:", balance)
+		fmt.Println("Rating:", rating)
+		fmt.Println("Married:", married)
+		fmt.Println("Birth Date:", birthDate)
+		fmt.Println("Created At:", createdAt)
 	}
 }
