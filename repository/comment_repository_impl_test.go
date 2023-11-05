b6208a0811cc38e1131e65897d294e8f57822dbb
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-05 13:10:10 +0700

    Add a test for inserting a comment into the repository

---
+++ repository/comment_repository_impl_test.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"golang_mysql"
+	"golang_mysql/entity"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestCommentInsert(t *testing.T) {
+	commentRepository := NewCommentRepository(golang_mysql.GetConnection())
+
+	ctx := context.Background()
+
+	comment := entity.Comment{
+		Email:   "repo@gmail.com",
+		Comment: "Hello from repository",
+	}
+
+	result, err := commentRepository.Insert(ctx, comment)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
+}
